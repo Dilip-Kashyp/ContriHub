aa02421fa9940046d26f311d166027ccca9c0f24
Dilip Kumar <dilipkumar.dev.official@gmail.com>
2026-04-23 23:02:08 +0530

    feat: add API_CONFIG constant with production base URL

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-  BASE_URL: "http://localhost:8080/api/v1",
+  // BASE_URL: "http://localhost:8080/api/v1",
+  BASE_URL: "https://url-shortener-be.duckdns.org/contrihube/api/v1",
 };
